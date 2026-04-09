169af4ccbb3b5764c8fbb306f911bb5b631a5e45
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-09 18:38:49 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -62,9 +62,6 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-                [
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
-        ],
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
