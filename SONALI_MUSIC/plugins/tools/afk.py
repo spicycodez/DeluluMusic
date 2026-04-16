7ed2bbba697487aafaeb377eb090a95db853d6b8
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-17 01:41:50 +0530

    Update afk.py

--- SONALI_MUSIC/plugins/tools/afk.py
+++ SONALI_MUSIC/plugins/tools/afk.py
@@ -16,22 +16,12 @@ NEXIO = [
         ]
 
 Sona = [
-    "https://graph.org/file/e509753cf069de86e52f8.jpg",
-    "https://graph.org/file/babb71b593f36549218ce.jpg",
-    "https://graph.org/file/4a254d425fb4bf09b7470.jpg",
-    "https://graph.org/file/51f37e3c2d4aaff5cf80e.jpg",
-    "https://graph.org/file/df01978f91c14b16292f1.jpg",
-    "https://graph.org/file/a6e3e9d54c8b2e01787b6.jpg",
-    "https://graph.org/file/49bcbc23be713fbe06bac.jpg",
-    "https://graph.org/file/809651f9be99ee2bf76ab.jpg",
-    "https://graph.org/file/134c9f52f4ba0f7691cd1.jpg",
-    "https://graph.org/file/4b5c2174d7f38b4b4abd7.jpg",
-    "https://graph.org/file/80feff5bb4a03cf331945.jpg",
-    "https://graph.org/file/0379defeb51910065beac.jpg",
-    "https://graph.org/file/323b07bccd5e5e1f81f61.jpg",
-    "https://graph.org/file/cbe5c31b9ea5220b17969.jpg",
-    "https://graph.org/file/1a4e7071b3e64c620e003.jpg",
-    "https://graph.org/file/d37dd94135f355f9b6866.jpg",
+    "https://litter.catbox.moe/oqubzzgpktlchlh8.jpg",
+    "https://litter.catbox.moe/e9hanjhqq7uqh98m.jpg",
+    "https://litter.catbox.moe/oxrmgvcfsdapltol.jpg",
+    "https://litter.catbox.moe/cwz4lg1haykxbbv4.jpg",
+    "https://litter.catbox.moe/0d2xs7929wyi74vs.jpg",
+    
     ]
 
 @app.on_message(filters.command(["afk", "off", "bye"], prefixes=["/", "!", "."]))
