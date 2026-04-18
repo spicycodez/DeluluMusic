0b85ba620357df04623000746f2153b0f47fc2b7
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-18 11:22:00 +0530

    Update Youtube.py

--- SONALI_MUSIC/platforms/Youtube.py
+++ SONALI_MUSIC/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch, Playlist
+from youtubesearchpython.__future__ import VideosSearch
 from SONALI_MUSIC.utils.formatters import time_to_seconds
 import aiohttp
 from SONALI_MUSIC import LOGGER
