db1302ac5e99fcd0d733aa43928369eef9d405bf
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-18 18:28:49 +0530

    Update ping.py

--- SONALI_MUSIC/plugins/tools/ping.py
+++ SONALI_MUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://files.catbox.moe/plxzb4.mp4",
+        video="https://litter.catbox.moe/zmybs6tifcxdpl0r.mp4",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await Sona.ping()
