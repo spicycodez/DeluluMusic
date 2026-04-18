2d4988c4609ad2ae56fe16141c706d9c5d6bf4fe
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-04-18 15:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 #API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
 #VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-YT_API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+YT_API_KEY = getenv("API_KEY", "xbit_issQbNpQKwoZaRVBQtEogF1kcbVorV4I") # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 YTPROXY_URL = getenv("YTPROXY_URL", None)
 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "")
